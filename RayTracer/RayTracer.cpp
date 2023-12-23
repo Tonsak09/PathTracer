bd97299e38520d3cf66500425311bca7a2ac2feb
Narai Risser <89745834+NGR7989@users.noreply.github.com>
2023-12-23 14:22:16 -0500

    Camera with blue gradient

--- RayTracer/RayTracer.cpp
+++ RayTracer/RayTracer.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
+
+color ray_color(const ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    double a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
     // Image
 
+    double aspect_ratio = 16.0 / 9.0;
     int image_width = 256;
-    int image_height = 256;
+    
+    // Calc height based from ratio and meets min of 1 
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    
+
+    #pragma region Camera
+
+    double focal_length = 1.0f;
+    double viewport_height = 2.0;
+    // Use the ratio between image height and image width to find width but not
+    // the aspect ratio variable since that's the ideal ratio and possibly not 
+    // the real ratio  
+    double viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+    vec3 camera_center = point3(0, 0, 0);
+
+    // Vectors for horizontal and vertical viewport edges 
+    // Remember that we draw from top of the screen to bottom! This is why the
+    // vertical vector is a negative value rather than positive 
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calc the chance between pixels
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    // Calc top left pixel 
+    vec3 viewport_upper_left = camera_center
+                             - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    // We start in the CENTER of the first pixel not its corner 
+    vec3 pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    #pragma endregion
+
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -17,21 +59,13 @@ int main() {
         std::clog << "\rScaleLines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) 
         {
+            vec3 pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            vec3 ray_direction = pixel_center - camera_center;
 
-            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
-            /*auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            color pixel_color = color(r, g, b);
-            write_color(std::cout, pixel_color);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';*/
         }
     }
 
